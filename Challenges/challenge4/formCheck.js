cbb330e84405dd8c19a0ecdf611de0fa8c717940
Uriahx <uriah.squarcina@vtsd.com>
2024-01-08 13:54:50 -0500

    boh

---
+++ Challenges/challenge4/formCheck.js
@@ -0,0 +1,26 @@
+// Gain Access to form elements
+const submitButton = document.getElementById('submit');
+const nameField= document.getElementById("name");
+const emailField= document.getElementById("email");
+const phoneField= document.getElementById("phone");
+
+// Add event listener to submit button
+submitButton.addEventListener('click', checkForm);
+name.addEventListener("click", changeBG);
+email.addEventListener("click", changeBG);
+phone.addEventListener("click", changeBG);
+
+
+function checkForm(){
+    /*
+        In this method check whether name, email, and phone number have input.
+        If they are blank turn the fields red.  Otherwise do nothing.
+     */
+
+    if(nameField.value=== '')
+
+
+
+
+    alert("It is hooked up!");
+}
